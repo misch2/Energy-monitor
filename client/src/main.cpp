9338dd4ce473db57afeb46cbd2dcecc9ca5f2a34
Michal Schwarz <michal.schwarz@gmail.com>
2024-04-19 22:04:29 +0200

    diagnostics

--- client/src/main.cpp
+++ client/src/main.cpp
@@ -89,7 +89,7 @@ void my_print(const char* buf) {
 
 void wdtInit() {
 #ifdef USE_WDT
-  TRACE_PRINT("Configuring WDT for %d seconds", WDT_TIMEOUT);
+  DEBUG_PRINT("Configuring WDT for %d seconds", WDT_TIMEOUT);
   esp_task_wdt_init(WDT_TIMEOUT, true);  // enable panic so ESP32 restarts
   esp_task_wdt_add(NULL);                // add current thread to WDT watch
 #endif
@@ -97,17 +97,68 @@ void wdtInit() {
 
 void wdtRefresh() {
 #ifdef USE_WDT
-  TRACE_PRINT("(WDT ping)");
+  // TRACE_PRINT("(WDT ping)");
   esp_task_wdt_reset();
 #endif
 }
 
 void wdtStop() {
 #ifdef USE_WDT
-  TRACE_PRINT("Stopping WDT...");
+  // TRACE_PRINT("Stopping WDT...");
   esp_task_wdt_deinit();
 #endif
 }
+
+String resetReasonAsString() {
+  esp_reset_reason_t reset_reason = esp_reset_reason();
+  if (reset_reason == ESP_RST_UNKNOWN) {
+    return "UNKNOWN";
+  } else if (reset_reason == ESP_RST_POWERON) {
+    return "POWERON";
+  } else if (reset_reason == ESP_RST_SW) {
+    return "SW";
+  } else if (reset_reason == ESP_RST_PANIC) {
+    return "PANIC";
+  } else if (reset_reason == ESP_RST_INT_WDT) {
+    return "INT_WDT";
+  } else if (reset_reason == ESP_RST_TASK_WDT) {
+    return "TASK_WDT";
+  } else if (reset_reason == ESP_RST_WDT) {
+    return "WDT";
+  } else if (reset_reason == ESP_RST_DEEPSLEEP) {
+    return "DEEPSLEEP";
+  } else if (reset_reason == ESP_RST_BROWNOUT) {
+    return "BROWNOUT";
+  } else if (reset_reason == ESP_RST_SDIO) {
+    return "SDIO";
+  } else {
+    return "? (" + String(reset_reason) + ")";
+  }
+};
+
+String wakeupReasonAsString() {
+  esp_sleep_wakeup_cause_t wakeup_reason = esp_sleep_get_wakeup_cause();
+  if (wakeup_reason == ESP_SLEEP_WAKEUP_UNDEFINED) {
+    return "UNDEFINED";
+  } else if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT0) {
+    return "EXT0";
+  } else if (wakeup_reason == ESP_SLEEP_WAKEUP_EXT1) {
+    return "EXT1";
+  } else if (wakeup_reason == ESP_SLEEP_WAKEUP_TIMER) {
+    return "TIMER";
+  } else if (wakeup_reason == ESP_SLEEP_WAKEUP_TOUCHPAD) {
+    return "TOUCHPAD";
+  } else if (wakeup_reason == ESP_SLEEP_WAKEUP_ULP) {
+    return "ULP";
+  } else {
+    return "? (" + String(wakeup_reason) + ")";
+  }
+};
+
+void logResetReason() {
+  DEBUG_PRINT("Reset reason: %s", resetReasonAsString());
+  DEBUG_PRINT("Wakeup reason: %s", wakeupReasonAsString());
+};
 
 void publish_homeassistant_value(bool startup,                // true if the device is starting up, false if the value is changing
                                  String component,            // component type e.g. "sensor", "text", "switch", etc.
@@ -572,6 +623,7 @@ void setup() {
 #endif
   wdtInit();
 
+  logResetReason();
 
 // #ifdef USE_WDT
 //   if (esp_reset_reason() == ESP_RST_TASK_WDT) {
